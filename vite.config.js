acefd066bc46ac6cff4c4067baa3859f2a00a193
SaKurA2152 <c1038341@planet.kanazawa-it.ac.jp>
2022-09-12 23:59:59 +0900

    ReactのサンプルDOMの描画

--- vite.config.js
+++ vite.config.js
@@ -12,8 +12,7 @@ export default defineConfig({
             refresh: true,
         }),
         react(),
-    ],
-    server: {
+    ],server: {
         host: '0.0.0.0',
         hmr: {
             host: 'localhost',
